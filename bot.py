c6afb101a51409c4d6435da3e363bf5ed7c4ab7c
nishantbalaji <44332326+NishantBalaji@users.noreply.github.com>
2020-08-09 03:23:54 -0700

    echo and hi command

--- bot.py
+++ bot.py
@@ -1,4 +1,37 @@
-import discord
+import os
 import json
+from discord.ext import commands
+from dotenv import load_dotenv
+import random
+#import regex
 
-Client client = new client();
+load_dotenv()
+KEY = os.getenv('API_KEY')
+TOKEN = os.getenv('DISCORD_TOKEN')
+#client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name='hi')
+async def test(ctx):
+
+    await ctx.send("hello")
+
+
+
+@bot.command(name='echo')
+async def echo(ctx, *args):
+    output = " "
+    for words in args:
+        output += words
+        output += " "
+    await ctx.message.delete()
+    await ctx.send(output)
+
+
+
+bot.run(TOKEN)
